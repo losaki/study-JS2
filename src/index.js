bf3ea80cf5004b6e1e49ce4b921665cc57e819f5
losaki <95681771+losaki@users.noreply.github.com>
2022-07-16 14:09:46 +0900

    削除機能

--- src/index.js
+++ src/index.js
@@ -13,8 +13,26 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  //button完了生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("aa");
+  });
+
+  //button削除生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //押された削除ボタンの親divを未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
   //divの子要素に各要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   //未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(div);
